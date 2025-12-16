173c1d273c566b2a06b9cd0792e99ae5158436f4
Roshan Mallick <roshanmallick2025@gmail.com>
2025-12-17 00:04:22 +0530

    Add even/odd check program in C

---
+++ divisible.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is divisible by 2\n", num);
+    }
+    else {
+        printf("%d is not divisible by 2\n", num);
+    }
+
+    return 0;
+}
+
