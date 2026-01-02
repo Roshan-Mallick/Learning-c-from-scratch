a15159d0ea0311f969492973b691db670c614f26
Roshan Mallick <roshanmallick2025@gmail.com>
2026-01-02 21:14:20 +0530

    print table in reverse order

---
+++ reverse_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int input;
+    printf("Enter input : ");
+    scanf("%d", &input);
+
+    int table_upto;
+    printf("Enter table upto : ");
+    scanf("%d", &table_upto);
+
+    int table;
+
+    for (int i = table_upto; i >= 1; i--) {
+        table = input * i;
+        printf("%d x %d = %d\n", input, i, table);
+    }
+
+    return 0;
+}
+
