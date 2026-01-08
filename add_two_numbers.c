858c419f77abe7b58e86a47ef782b416adada7b9
Roshan Mallick <roshanmallick2025@gmail.com>
2026-01-08 21:41:05 +0530

    Add function to calculate sum of two integers

---
+++ add_two_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Function declaration
+int add(int a, int b);
+
+int main() {
+    int result = add(5, 3);
+    printf("Sum = %d\n", result);
+    return 0;
+}
+
+// Function definition
+int add(int a, int b) {
+    return a + b;
+}
+
