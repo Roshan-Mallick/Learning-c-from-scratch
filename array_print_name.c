2b797f97e04d25e4a2213a3b14c9a91a6cb8f03c
Roshan Mallick <roshanmallick2025@gmail.com>
2026-01-12 01:36:56 +0530

    practice: add array-based name input and printing program

---
+++ array_print_name.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char firstName [10];
+
+    printf("Enter you first name : ");
+
+    scanf("%9s", firstName);
+
+    char lastName [10];
+
+    printf("Enter you first name : ");
+
+    scanf("%9s", lastName);
+
+
+    printf("Your name is : %s %s \n : ", firstName , lastName);
+    
+    
+
+    return 0;
+}
+
